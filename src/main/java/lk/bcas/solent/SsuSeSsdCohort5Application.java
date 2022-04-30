cc88b30548184e6b39e9efa1ef2defe6cb8d6c2c
Afrar Malakooth <mmafrar@gmail.com>
2022-04-30 12:39:50 +0530

    Setting up the development environment

---
+++ src/main/java/lk/bcas/solent/SsuSeSsdCohort5Application.java
@@ -0,0 +1,13 @@
+package lk.bcas.solent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuSeSsdCohort5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuSeSsdCohort5Application.class, args);
+	}
+
+}
---
+++ src/test/java/lk/bcas/solent/SsuSeSsdCohort5ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.bcas.solent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuSeSsdCohort5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
